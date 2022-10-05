31e1a64d668f4a8ede93419dfe814e40c3fc9d70
akhror <zhunaevaxror@gmail.com>
2022-10-05 21:14:24 +0500

    final

---
+++ main.js
@@ -0,0 +1,26 @@
+var str = prompt("Введите ваше имя")
+console.log('Меня зовут ' + str);
+
+var str = prompt('Введите свой возраст')
+console.log('Мне ' + str);
+
+var str = prompt('Решите пример 6 + 3 = ?')
+console.log('Пример 1: 6 + 3 = 9 Ваш ответ ' + str);
+
+
+var str = prompt('Решите пример 20 - 5 = ?')
+console.log('Пример 2: 20 - 5 = 15 Ваш ответ ' + str);
+
+
+var str = prompt('Решите пример 15 * 2 = ?')
+console.log('Пример 3: 15 * 2 = 30 Ваш ответ ' + str);
+
+
+var str = prompt('Решите пример 8 / 4 = ?')
+console.log('Пример 4: 8 / 4 = 2 Ваш ответ ' + str);
+
+
+var str = prompt('Решите пример 10 % 3 = ?')
+console.log('Пример 5: 10 % 3 = 1 Ваш ответ ' + str);
+
+alert('Откройте консоль для просмотра')
